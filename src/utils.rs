5f41d704e6b23afb2cd66693358be70af6f1586a
fpieper <florian@pieper.ai>
2024-08-16 12:49:42 +0200

    Add assert_component_is_approved

--- src/utils.rs
+++ src/utils.rs
@@ -26,3 +26,36 @@ pub fn sort_buckets(a_bucket: Bucket, b_bucket: Bucket) -> (Bucket, Bucket) {
     }
     (b_bucket, a_bucket)
 }
+
+pub fn assert_component_is_approved(namespace: &str, component_address: ComponentAddress) {
+    let blueprint_id = ScryptoVmV1Api::object_get_blueprint_id(&component_address.into());
+
+    let metadata_key = build_blueprint_metadata_key(namespace, &blueprint_id);
+    let metadata_package_address = package_address_from_metadata(&metadata_key);
+
+    assert_eq!(
+        Some(blueprint_id.package_address),
+        metadata_package_address,
+        "The package address of {} is not approved under the key {} in the pool package metadata.",
+        Runtime::bech32_encode_address(component_address),
+        metadata_key
+    );
+}
+
+pub fn package_address_from_metadata(key: &str) -> Option<PackageAddress> {
+    let own_package: Package = Runtime::package_address().into();
+    let metadata_value: GlobalAddress = own_package.get_metadata(key).ok()??;
+    metadata_value.try_into().ok()
+}
+
+pub fn build_blueprint_metadata_key(namespace: &str, blueprint_id: &BlueprintId) -> String {
+    let full_address = Runtime::bech32_encode_address(blueprint_id.package_address);
+    let short_address = &full_address[full_address.len() - 6..];
+
+    format!(
+        "packages.{}.{}.{}",
+        namespace,
+        blueprint_id.blueprint_name.to_lowercase(),
+        short_address
+    )
+}
